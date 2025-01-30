cfca0b0e2e5db07dd77dd07a8833089fba9b13e5
Cristian Zapata <theposidesign@gmail.com>
2025-01-30 16:54:09 +0100

    fix(server): rebuild server structure

--- src/server.c
+++ src/server.c
@@ -6,12 +6,11 @@
 /*   By: crizapat <crizapat@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:01:38 by crizapat          #+#    #+#             */
-/*   Updated: 2025/01/24 13:21:06 by crizapat         ###   ########.fr       */
+/*   Updated: 2025/01/30 16:49:45 by crizapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
-#include <signal.h>
 
 t_bits_buffer g_server_buffer = {0, 0};
 
@@ -23,32 +22,34 @@ static void reset_data_buffers()
 
 static void signal_handler(int signal)
 {
-	if (signal == SIGUSR1)
-		g_server_buffer.byte = (g_server_buffer.byte << 1) | 1;
-	else if (signal == SIGUSR2) 
-		g_server_buffer.byte = (g_server_buffer.byte << 1) | 0;
-	g_server_buffer.bit++;
+	char *message;
 
-	//
-    if (g_server_buffer.bit == 8) {
-        printf("%c", g_server_buffer.byte);
-        fflush(stdout); // Asegurar que se imprima inmediatamente
-        reset_data_buffers();
-    }
-	//
+	if (signal == SIGUSR1) {
+		g_server_buffer.bit = (g_server_buffer.bit << 1) | 1;	
+	}
+	if (signal == SIGUSR2) {
+		g_server_buffer.bit = (g_server_buffer.bit << 1) | 0;	
+	}
+	g_server_buffer.byte++;
 }
 
 int main ()
 {
 	int server_pid = getpid();
-	printf("[SERVER PID] --> [%d]\n", server_pid);
+	ft_printf("[SERVER PID] --> [%d]\n", server_pid);
 
-	struct sigaction sign;
-	sign.sa_handler = signal_handler;
+	struct sigaction sa;
+
+	sa.sa_handler = &signal_handler;
+	sa.sa_flags = SA_RESTART;
+	sigemptyset(&sa.sa_mask);
+
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 
 	while(1)
 	{
-		sleep(1);
+		pause();
 	}
 	return 0;
 }
