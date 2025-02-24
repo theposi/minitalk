a2fad64e505d7f16a175262d79478d5249c2dd7c
Cristian Zapata <theposidesign@gmail.com>
2025-02-24 15:33:30 +0100

    fix(server): norminette format improvement

--- src/server.c
+++ src/server.c
@@ -12,86 +12,86 @@
 
 #include "../minitalk.h"
 
-t_bits_buffer g_server_buffer = {0, 0, NULL};
+t_bits_buffer	g_server_buffer = {0, 0, NULL};
 
-static void reset_data_buffers()
+static void	reset_data_buffers(void)
 {
 	g_server_buffer.bit = 0;
 	g_server_buffer.byte = 0;
-} 
+}
 
-static char *append_char(char *str, char c)
+static char	*append_char(char *str, char c)
 {
-	int len;
+	int		len;
+	char	*new_str;
 
 	len = 0;
 	if (str)
 		len = ft_strlen(str);
-
-	char *new_str = malloc(len + 2);
+	new_str = malloc(len + 2);
 	if (!new_str)
 		return (NULL);
-
 	if (str)
 	{
 		ft_memcpy(new_str, str, len);
 		free(str);
 	}
-
 	new_str[len] = c;
 	new_str[len + 1] = '\0';
 	return (new_str);
 }
 
-static void signal_handler(int signal)
+static void	process_byte(void)
 {
-	if (signal == SIGUSR1)
-		g_server_buffer.byte = (g_server_buffer.byte << 1) | 1;	
-	else if (signal == SIGUSR2)
-		g_server_buffer.byte = (g_server_buffer.byte << 1) | 0;
+	char	*temp;
 
-	g_server_buffer.bit++;
-
-	if (g_server_buffer.bit == 8)
+	if (g_server_buffer.byte == '\0')
 	{
-		if (g_server_buffer.byte == '\0')
+		if (g_server_buffer.msg)
 		{
-			if (g_server_buffer.msg)
-			{
-				ft_printf("%s\n", g_server_buffer.msg);
-				free(g_server_buffer.msg);
-				g_server_buffer.msg = NULL;
-			}
+			ft_printf("%s\n", g_server_buffer.msg);
+			free(g_server_buffer.msg);
+			g_server_buffer.msg = NULL;
 		}
-		else
-		{
-			g_server_buffer.msg = append_char(g_server_buffer.msg, g_server_buffer.byte);
-			if (!g_server_buffer.msg)
-			{
-				ft_printf("Error: Memory allocation failed.\n");
-				exit(1);
-			}
-		}
-		reset_data_buffers();
 	}
+	else
+	{
+		temp = append_char(g_server_buffer.msg, g_server_buffer.byte);
+		if (!temp)
+		{
+			ft_printf("Error: Memory allocation failed.\n");
+			exit(1);
+		}
+		g_server_buffer.msg = temp;
+	}
+	reset_data_buffers();
 }
 
-int main (void)
+static void	signal_handler(int signal)
 {
-	server_banner();
-	int server_pid = getpid();
-	ft_printf("[SERVER PID] --> [%d]\n", server_pid);
+	if (signal == SIGUSR1)
+		g_server_buffer.byte = (g_server_buffer.byte << 1) | 1;
+	else if (signal == SIGUSR2)
+		g_server_buffer.byte = (g_server_buffer.byte << 1) | 0;
+	g_server_buffer.bit++;
+	if (g_server_buffer.bit == 8)
+		process_byte();
+}
 
-	struct sigaction sign;
+int	main(void)
+{
+	int					server_pid;
+	struct sigaction	sign;
+
+	server_pid = getpid();
+	server_banner();
+	ft_printf("[SERVER PID] --> [%d]\n", server_pid);
 	sign.sa_handler = &signal_handler;
 	sign.sa_flags = SA_RESTART;
 	sigemptyset(&sign.sa_mask);
-
 	sigaction(SIGUSR1, &sign, NULL);
 	sigaction(SIGUSR2, &sign, NULL);
-
-	while(1)
-		pause();
-
-	return 0;
+	while (1)
+		pause ();
+	return (0);
 }
